fca9cea2c9b6b4f0e4ff04fee752be9fb0e04eb0
Rafael Cardeal Lodi <65565453+rafaelcardeallodi@users.noreply.github.com>
2022-01-23 21:08:56 -0300

    Dynamic import (Code Splitting)

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,4 +1,14 @@
-import { memo } from 'react';
+import { memo, useState } from 'react';
+import dynamic from 'next/dynamic';
+import { AddProductToWishlistProps } from './AddProductToWishlist';
+
+// import { AddProductToWishlist } from './AddProductToWishlist';
+
+const AddProductToWishlist = dynamic<AddProductToWishlistProps>(() => {
+  return import('./AddProductToWishlist').then(mod => mod.AddProductToWishlist)
+}, {
+  loading: () => <span>Carregando...</span>
+})
 
 interface ProductItemProps{
   product: {
@@ -11,10 +21,19 @@ interface ProductItemProps{
 }
 
 function ProductItemComponent({ product, onAddToWishList }: ProductItemProps){
+  const [isAddingToWishList, setIsAddingToWishList] = useState(false);
+
   return (
     <div>
       {product.title} - <strong>{product.priceFormatted}</strong>
-      <button onClick={() => onAddToWishList(product.id)}>Add to wishlist</button>
+      <button onClick={() => setIsAddingToWishList(true)}>Adicionar aos favoritos</button>
+      
+      { isAddingToWishList && (
+        <AddProductToWishlist 
+        onAddToWishList={() => onAddToWishList(product.id)}
+        onRequestClose={() => setIsAddingToWishList(false)}
+        />
+      ) }
     </div>
   );
 }
